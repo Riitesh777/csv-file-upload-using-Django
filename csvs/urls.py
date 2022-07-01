c34d3d691d242db67beb07293de23e00fceb603f
Ritesh777 <riteshguleria23@gmail.com>
2022-07-01 21:27:06 +0530

    First commit

---
+++ csvs/urls.py
@@ -0,0 +1,9 @@
+from multiprocessing.spawn import import_main_path
+from django.urls import path
+from .views import upload_file_view
+ 
+app_name='csvs'
+
+urlpatterns = [
+    path('', upload_file_view, name='upload-view'),
+]
